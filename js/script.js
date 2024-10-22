0be45c1c59fbaf2715804562252b5af5ba450878
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-10-23 00:52:23 +0600

    theme round color cng , copyright added

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 const round = document.querySelector(".round");
 round.addEventListener("click", () => {
   const html = document.querySelector("html");
+  const themecheck = document.querySelector("#themecheck");
   html.classList.toggle("dark");
   html.classList.toggle("light");
+  themecheck.classList.toggle("themecheckcolor");
   document.querySelector(".dark").setAttribute("data-theme", "theme");
 });
 
