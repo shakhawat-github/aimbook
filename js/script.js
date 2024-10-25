8802c508ae1526b565ab94a853ac64202e0fcab4
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-10-25 08:04:05 +0600

    join section hide function added

--- js/script.js
+++ js/script.js
@@ -27,3 +27,11 @@ passfinder.addEventListener("click", () => {
 });
 // password show/hide end
 
+// hide join section start
+const hidejoin = document.querySelector('.hidejoin');
+const join = document.querySelector('#join')
+hidejoin.addEventListener('click' , ()=>{
+  join.style.display = 'none'
+})
+// hide join section end
+
