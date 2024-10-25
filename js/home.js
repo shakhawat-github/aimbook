a7d84ce4440f4758737d538b72d9b9f8707772d1
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-10-25 18:26:28 +0600

    nav bar added

--- js/home.js
+++ js/home.js
@@ -2,15 +2,23 @@
 const homesearch = document.querySelector(".homesearch");
 const homesearchbtn = document.querySelector(".homesearchbtn");
 
-// homesearchbtn.addEventListener("click", () => {
-//     homesearch.classList.toggle('homesearch-extend')
-//     homesearch.classList.toggle('homesearch-cut')
-// });
 homesearchbtn.addEventListener("click", () => {
-    
-    homesearch.classList.toggle('homesearch-extend')
-    homesearch.classList.toggle('homesearch-cut')
+  homesearch.classList.toggle("homesearch-extend");
+  homesearch.classList.toggle("homesearch-cut");
 });
 
-
 // home search end
+
+// navcollapse start
+const homenavigation = document.querySelector(".homenavigation");
+const brandbtn = document.querySelector(".brandbtn");
+const navcloser = document.querySelector(".navcloser");
+brandbtn.addEventListener("click", () => {
+  homenavigation.classList.remove("navbarhide");
+  homenavigation.classList.add("navbarshow");
+});
+navcloser.addEventListener("click", () => {
+  homenavigation.classList.add("navbarhide");
+  homenavigation.classList.remove("navbarshow");
+});
+// navcollapse end
